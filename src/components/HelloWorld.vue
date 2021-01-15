a75f2cc6a5de3164b0ef5f5b517c2310a0d3461d
Brandon Whisnant <galvatron@Galvatron.local>
2021-01-14 23:32:29 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,42 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(time, index) in event" :key="index">
+        {{ changeTime(time) }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { DateTime } from 'luxon'
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      date: '',
+      event: {
+        timeOne: DateTime.local(2021, 2, 4, 5, 30),
+        timeTwo: DateTime.local(2020, 4, 20)
+      },
+      timezone: 'Testing One',
+      timezoneTwo: 'Testing Two'
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    changeTime(time) {
+      if (this.timezone === 'Testing') {
+        return time.plus({ hours: 2}).toLocaleString(DateTime.TIME_24_SIMPLE)
+      }
+
+      if (this.timezoneTwo === 'Testing Two') {
+        return time.plus({ hours: 4}).toLocaleString(DateTime.TIME_24_SIMPLE)
+      }
+      
+    }
   }
 }
 </script>
@@ -49,8 +51,9 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
 }
 a {
   color: #42b983;
